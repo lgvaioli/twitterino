ae22e01313aa0d6fc0e2ff6f45b02e0abf17fdcb
Laureano G. Vaioli <laureano3400@gmail.com>
2020-08-08 14:24:23 -0300

    Add React's /build folder to version control
    This is just the easiest way to get the files into Heroku.
    
    Minor changes:
      - Delete npm build script, because we don't want Heroku to
      try to run a React build

---
+++ frontend/build/precache-manifest.c1fad012f3f99b37c2a4d0984fed2598.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6dc9068f78e5a81005a82c8707312b40",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d18534a4763274c2ef9b",
+    "url": "/static/css/main.f226b60a.chunk.css"
+  },
+  {
+    "revision": "bbd41b33de026df98d4c",
+    "url": "/static/js/2.f5c4cc8b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f5c4cc8b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d18534a4763274c2ef9b",
+    "url": "/static/js/main.0ec762c8.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
